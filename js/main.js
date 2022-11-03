eca2e3c059a11db2e03dd27b7f05c76c3cc66309
Cauê Reple <caue-reple@hotmail.com>
2022-11-03 19:40:48 +0100

    conditions and notifications

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById('lista')
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+const itens = JSON.parse(localStorage.getItem("itens")) || [];// usamos o JSON.parse para transformar de volta o nosso objeto transformado em string lá embaixo, em objeto
 
-console.log(itens)
-console.log([])
 
 itens.forEach( elemento => {
-    console.log(elemento.nome, elemento.quantidade)
+    criaElemento(elemento)
 })
 
 
@@ -15,14 +13,36 @@ form.addEventListener("submit", evento => {
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
+    const requerimento = document.querySelector(".requerimento")
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if(nome.value != "" && quantidade.value != "") {
+        criaElemento(itemAtual)
+        
+        // push serve para inserir algo dentro de um array
+        itens.push(itemAtual)
+
+        localStorage.setItem("itens", JSON.stringify(itens)) //JSON.stringfy transforma o objeto em uma string
+
+        form.removeChild(requerimento)
+
+    } else {
+        if(!requerimento){
+            criaParagrafo()
+        }
+    }
 
     nome.value = ""
     quantidade.value = ""
+
+    nome.focus()
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
     // <li class="item"><strong>7</strong>Camisa</li>
     const novoItem = document.createElement('li')
@@ -31,21 +51,19 @@ function criaElemento(nome, quantidade) {
     novoItem.classList.add("item")
     
     // innerHTML recebe o que está dentro da variável quantidade, ou seja, aquilo que estamos escrevendo dentro do input quantidade na nossa página
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
     // appendChild nos deixa colocar um elemento HTML dentro de outro. No nosso caso, queremos que a tag <strong> fique dentro da tag <li>
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
     
     lista.appendChild(novoItem)
+}
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-    
-    // push serve para inserir algo dentro de um array
-    itens.push(itemAtual)
+function criaParagrafo() {
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add("requerimento")
+    const texto = document.createTextNode("Você deve escrever alguma coisa!")
 
-    localStorage.setItem("itens", JSON.stringify(itens)) //JSON.stringfy transforma o objeto em uma string
-
+    form.appendChild(paragrafo)
+    paragrafo.appendChild(texto)
 }
